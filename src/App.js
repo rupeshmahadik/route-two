1de5cb7c1f5795a357953daa0d71f2675cf886a6
Rupem38 <40014564+rupeshmahadik@users.noreply.github.com>
2023-08-09 16:51:27 +0530

    removed date

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let time = new Date()
   return (
     <div className="App">
       <h2>Demo 02 - Netlify</h2>
       <p>lorem</p>
       <h2>time : </h2>
-      <h3>{time}</h3>
     </div>
   );
 }
