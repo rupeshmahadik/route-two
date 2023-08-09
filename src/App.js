d9d4670e967204f12bfd1257d66649f7108773a8
Rupem38 <40014564+rupeshmahadik@users.noreply.github.com>
2023-08-09 16:58:22 +0530

    updated time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 function App() {
+  let time = new Date().getSeconds()
   return (
     <div className="App">
       <h2>Demo 02 - Netlify</h2>
       <p>lorem</p>
-      <h2>time : </h2>
+      <h2>time : {time}</h2>
     </div>
   );
 }
