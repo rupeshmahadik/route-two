42dce1d36054e5e4ab44d78ad0b33b11fa399923
Rupem38 <40014564+rupeshmahadik@users.noreply.github.com>
2023-08-09 16:46:38 +0530

    added time

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let time = new Date()
   return (
     <div className="App">
       <h2>Demo 02 - Netlify</h2>
       <p>lorem</p>
+      <h3>{time}</h3>
     </div>
   );
 }
